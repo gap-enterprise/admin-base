815d472605020d6f5dc74276bdd0ea23acd73b99
Brou Marcel <broumarcel87@gmail.com>
2022-04-21 08:26:41 +0000

    #67 : Migrate DbProfiles to jOOQ -- v2

--- src/test/java/io/surati/gap/admin/base/db/DbProfilesTest.java
+++ src/test/java/io/surati/gap/admin/base/db/DbProfilesTest.java
@@ -19,16 +19,12 @@ package io.surati.gap.admin.base.db;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
-import javax.management.InvalidApplicationException;
-
 import org.cactoos.list.ListOf;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.llorllale.cactoos.matchers.Satisfies;
 
 import com.lightweight.db.EmbeddedPostgreSQLDataSource;
 import io.surati.gap.admin.base.api.Profile;
@@ -61,40 +57,32 @@ final class DbProfilesTest {
     /**
      * Profile
      */
-    private Profile profile;
+    private Profile adminpro;
     
 
     @BeforeEach
     void setUp() {
         this.profiles = new DbProfiles(this.src);
-        this.profile = new DbProfile(this.src, 1L);
+        this.adminpro = new DbProfile(this.src, 1L);
     }
     
 	@Test
-	public Profile getsProfileById() {
-		final Profile profile = this.profiles.get(1L);
+	public void getsProfileById() {
 		MatcherAssert.assertThat(
-			profile,
-			new Satisfies<>(
-				prf -> prf.id().equals(this.profile.id()) &&
-				prf.name().equals(this.profile.name()) &&
-				prf.accesses().equals(this.profile.accesses())
-			)
+				this.profiles.get(1L).id(),
+				Matchers.equalTo(1L)
 		);
-		return this.profiles.get(this.profile.id());
 	}
 	
 	@Test
-	public Profile addsProfile() {
+	public void addsProfile() {
 		final String name = "Guest";
 		final Profile profile = this.profiles.add(name);
 		MatcherAssert.assertThat(
-			profile,
-			new Satisfies<>(
-				prf -> prf.name().equals(name)
-			)
+				"Profile should contains Guest.",
+				new ListOf<>(this.profiles.iterate()).stream().anyMatch(p -> p.name().equals(profile.name())),
+	            Matchers.is(true)
 		);
-		return this.profiles.get(profile.id());
 	}
 	
 	@Test
@@ -107,35 +95,29 @@ final class DbProfilesTest {
 			final String name = names[idx];
 			MatcherAssert.assertThat(
 				"Profiles should match in ascending order.",
-				profile,
-				new Satisfies<>(
-					prf -> prf.name().equals(name)
-				)
+				profile.name(),
+				Matchers.equalTo(name)
 			);
 			idx ++;
 		}
     }
 	
 	@Test
-    void removesUnsedProfile() {
-		final Profile newProfile =  this.profiles.add("Guest");
-		this.profiles.remove(newProfile.id());
+    void removesUnusedProfile() {
+		final Profile newpro =  this.profiles.add("Guest");
+		this.profiles.remove(newpro.id());
 		MatcherAssert.assertThat(
             "Profile should be deleted.",
-            new ListOf<>(this.profiles.iterate()).stream().noneMatch(p -> p.id().equals(newProfile.id())),
+            new ListOf<>(this.profiles.iterate()).stream().noneMatch(p -> p.id().equals(newpro.id())),
             Matchers.is(true)
         );
     }
 	
 	@Test
     void removesProfileAlreadyUsed() {
-		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-	        this.profiles.remove(this.profile.id());
+		final Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+	        this.profiles.remove(this.adminpro.id());
 	    });
-
-	    String expectedMessage = "Le profil ne peut pas être supprimé (déjà utilisé) !";
-	    String actualMessage = exception.getMessage();
-
-	    assertTrue(actualMessage.contains(expectedMessage));
+	    assertTrue(exception.getMessage().contains("Le profil ne peut pas être supprimé (déjà utilisé) !"));
     }
 }
